9ca023d397e04e8c56b19ed20674f1e2211684a1
natakers <n.kersnovskaya@gmail.com>
2021-08-23 11:08:24 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
+	var a = str.split('');
+	var b = [];
+	const op_brack = ["(", '[', "{"];
+	const brackets = {
+		[')'] : "(",
+		[']'] : "[",
+		['}'] : "{",
+
+	}
+	console.log(str);
+	for (var i = 0; i <str.length; i++) {
+		if (op_brack.includes(str[i])) {
+			b.push(str[i]);
+			// console.log(b);
+		}
+		else {
+			if (b.length === 0) {
+				return false;
+			}
+		
+			var c = b[b.length-1];
+			// console.log(b.length);
+			if (brackets[str[i]] === c) {
+				b.pop();
+				// console.log(b);
+			}
+			else {
+				return false;
+			}
+		}
+	}
+	console.log(b);
+	return b.length === 0;
+
   // your solution
 }
